3b5e590acf75b44735f93c05bb6ae4854c6bf830
twntysmth <c.kamp6696@gmail.com>
2023-02-12 21:01:18 +0100

    Intro
    Build an Intro module

---
+++ storyblok/Intro.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+    const props = defineProps({ blok: Object });
+
+</script>
+
+<template>
+
+    <div class="intro">
+        <div class="grid-wrap" v-editable="blok">
+        
+            <div class="intro__video w12 mw24">
+                <figure>
+                    <video :src="blok.video.filename" :alt="blok.video.alt" autoplay muted loop></video>
+                </figure>
+            </div>
+
+            <h1 v-if="blok.headline" class="intro__headline w12 mw24">
+                {{ blok.headline }}
+            </h1>
+
+        </div>
+    </div>
+
+</template>
