44d8e9be3ec064399de41032d3dedc832b28a5d9
twntysmth <c.kamp6696@gmail.com>
2023-02-12 20:31:19 +0100

    Quote
    Build a Quote module

---
+++ storyblok/Quote.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+    const props = defineProps({ blok: Object });
+    const content = computed(() => renderRichText(props.blok.richtext));
+
+</script>
+
+<template>
+
+    <div class="quote">
+        <div class="grid-wrap" v-editable="blok">
+        
+            <div class="quote__content w12 mw18 mw-start-4 lw14 lw-start-6" v-html="content"></div>
+
+        </div>
+    </div>
+
+</template>
